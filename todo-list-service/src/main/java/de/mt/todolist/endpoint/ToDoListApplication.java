b38b3c5804b09daf8eeb49c9805cfc8ea1d1feab
Mario Trageser <mtrageser@MB045.local>
2021-06-30 17:41:19 +0200

    Added a REST resource

---
+++ todo-list-service/src/main/java/de/mt/todolist/endpoint/ToDoListApplication.java
@@ -0,0 +1,8 @@
+package de.mt.todolist.endpoint;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/todo-list-application")
+public class ToDoListApplication extends Application {
+}
