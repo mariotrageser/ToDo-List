cbbed4dca167c269baf0e820f777103270603879
Mario Trageser <mtrageser@MB045.local>
2021-07-07 12:19:25 +0200

    Added styling

--- todo-list-frontend/src/app/app.module.ts
+++ todo-list-frontend/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './component/app/app.component';
 import { TodoListComponent } from './component/todo-list/todo-list.component';
 import {HttpClientModule} from "@angular/common/http";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from "@angular/material/card";
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import {HttpClientModule} from "@angular/common/http";
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
